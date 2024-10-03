dc43953fc8508a0e4a6d12c91e9f155021244ab3
Thanush <115856620+Thanush101@users.noreply.github.com>
2024-10-03 17:23:42 +0530

    Update settings.py

--- cars/cars/cars/settings.py
+++ cars/cars/cars/settings.py
@@ -28,7 +28,7 @@ DEBUG = True
 ALLOWED_HOSTS = [
      'localhost',
     '127.0.0.1',
-    'gascasestudy-production.up.railway.app',
+    'shimmering-rebirth-production.up.railway.app',
 ]
 
 MEDIA_URL = '/media/'
